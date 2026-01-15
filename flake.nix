cb2b03bcacc025e7d3e54c64434e3be81be54f54
Tomás Palma <up202108880@edu.fe.up.pt>
2026-01-15 11:42:03 +0000

    chore: add nix flake

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+
+        rustNightly = pkgs.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override {
+          extensions = [ "rust-src" "rust-analyzer" "clippy" ];
+        });
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            rustNightly
+            pkgs.pkg-config
+          ];
+
+          shellHook = ''
+            echo "Loaded Rust Nightly: $(rustc --version)"
+          '';
+        };
+      }
+    );
+}
