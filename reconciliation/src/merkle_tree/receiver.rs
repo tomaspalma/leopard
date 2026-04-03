770dec740aacc49741df00c153414b27b279026f
Tomás Palma <up202108880@edu.fe.up.pt>
2026-04-04 00:51:04 +0100

    refactor: separate response in each match arm into separate functions

--- reconciliation/src/merkle_tree/receiver.rs
+++ reconciliation/src/merkle_tree/receiver.rs
@@ -40,83 +40,10 @@ impl ReceiveMerkleTreeMessageTask {
     fn handle_message(&self, msg: &MerkleTreeMessage, neighbor: NodeAddress) {
         match msg.wrapper() {
             MerkleTreeMessageWrapper::SyncRoot(root_hash) => {
-                let local_root = self.tree.get_root_hash();
-                if local_root != *root_hash {
-                    info!("Root hash mismatch. Requesting children nodes sync.");
-
-                    let state_clone = self.state.clone();
-                    let id_clone = self.identifier.connection_info().clone();
-                    let neighbor_clone = neighbor.clone();
-                    let protocol_id = msg.protocol();
-
-                    spawn!({
-                        let request_left = MerkleTreeMessage::new(
-                            protocol_id,
-                            MerkleTreeMessageType::new(
-                                MerkleTreeMessageTypeValues::SyncNodeRequest,
-                            ),
-                            MerkleTreeMessageWrapper::SyncNodeRequest("root-left".to_string()),
-                        );
-                        let _ = state_clone
-                            .send_through_socket(
-                                id_clone.clone(),
-                                Box::new(neighbor_clone.clone()),
-                                Box::new(request_left),
-                            )
-                            .await;
-
-                        let request_right = MerkleTreeMessage::new(
-                            protocol_id,
-                            MerkleTreeMessageType::new(
-                                MerkleTreeMessageTypeValues::SyncNodeRequest,
-                            ),
-                            MerkleTreeMessageWrapper::SyncNodeRequest("root-right".to_string()),
-                        );
-                        let _ = state_clone
-                            .send_through_socket(
-                                id_clone,
-                                Box::new(neighbor_clone),
-                                Box::new(request_right),
-                            )
-                            .await;
-                    });
-                } else {
-                    info!("Root hash match. In sync.");
-                }
+                self.handle_sync_root(msg.protocol(), neighbor, root_hash);
             }
             MerkleTreeMessageWrapper::SyncNodeRequest(node_id) => {
-                info!("Received SyncNodeRequest from {:?}", node_id);
-                let root_hash = self.tree.get_root_hash();
-
-                let (node_hash, parent_hash, node_key) =
-                    if let Some((node, p_hash)) = self.tree.get_node(node_id) {
-                        (node.hash, p_hash, node.key)
-                    } else {
-                        ([0; 32], None, None)
-                    };
-
-                let state_clone = self.state.clone();
-                let id_clone = self.identifier.connection_info().clone();
-                let neighbor_clone = neighbor.clone();
-                let protocol_id = msg.protocol();
-                let node_id_clone = node_id.clone();
-
-                spawn!({
-                    let response = MerkleTreeMessage::new(
-                        protocol_id,
-                        MerkleTreeMessageType::new(MerkleTreeMessageTypeValues::SyncNodeResponse),
-                        MerkleTreeMessageWrapper::SyncNodeResponse(
-                            node_id_clone,
-                            root_hash,
-                            node_hash,
-                            parent_hash,
-                            node_key,
-                        ),
-                    );
-                    let _ = state_clone
-                        .send_through_socket(id_clone, Box::new(neighbor_clone), Box::new(response))
-                        .await;
-                });
+                self.handle_sync_node_request(msg.protocol(), neighbor, node_id);
             }
             MerkleTreeMessageWrapper::SyncNodeResponse(
                 node_id,
@@ -125,115 +52,201 @@ impl ReceiveMerkleTreeMessageTask {
                 _parent_hash,
                 remote_key,
             ) => {
-                info!("Received SyncNodeResponse for {:?}", node_id);
-                let local_node = self.tree.get_node(node_id);
-                let local_hash = local_node.map_or([0; 32], |(n, _)| n.hash);
-
-                if local_hash != *hash {
-                    let state_clone = self.state.clone();
-                    let id_clone = self.identifier.connection_info().clone();
-                    let neighbor_clone = neighbor.clone();
-                    let protocol_id = msg.protocol();
-                    let node_id_clone = node_id.clone();
-
-                    if let Some(key) = remote_key {
-                        info!("Leaf node mismatch. Requesting data for key: {:?}", key);
-                        let key_clone = key.clone();
-                        spawn!({
-                            let request = MerkleTreeMessage::new(
-                                protocol_id,
-                                MerkleTreeMessageType::new(
-                                    MerkleTreeMessageTypeValues::DataRequest,
-                                ),
-                                MerkleTreeMessageWrapper::DataRequest(key_clone),
-                            );
-                            let _ = state_clone
-                                .send_through_socket(
-                                    id_clone,
-                                    Box::new(neighbor_clone),
-                                    Box::new(request),
-                                )
-                                .await;
-                        });
-                    } else if *hash != [0; 32] {
-                        info!(
-                            "Internal node mismatch at {:?}. Requesting children.",
-                            node_id
-                        );
-                        spawn!({
-                            let request_left = MerkleTreeMessage::new(
-                                protocol_id,
-                                MerkleTreeMessageType::new(
-                                    MerkleTreeMessageTypeValues::SyncNodeRequest,
-                                ),
-                                MerkleTreeMessageWrapper::SyncNodeRequest(format!(
-                                    "{}-left",
-                                    node_id_clone
-                                )),
-                            );
-                            let _ = state_clone
-                                .send_through_socket(
-                                    id_clone.clone(),
-                                    Box::new(neighbor_clone.clone()),
-                                    Box::new(request_left),
-                                )
-                                .await;
-
-                            let request_right = MerkleTreeMessage::new(
-                                protocol_id,
-                                MerkleTreeMessageType::new(
-                                    MerkleTreeMessageTypeValues::SyncNodeRequest,
-                                ),
-                                MerkleTreeMessageWrapper::SyncNodeRequest(format!(
-                                    "{}-right",
-                                    node_id_clone
-                                )),
-                            );
-                            let _ = state_clone
-                                .send_through_socket(
-                                    id_clone,
-                                    Box::new(neighbor_clone),
-                                    Box::new(request_right),
-                                )
-                                .await;
-                        });
-                    }
-                }
+                self.handle_sync_node_response(msg.protocol(), neighbor, node_id, hash, remote_key);
             }
             MerkleTreeMessageWrapper::DataRequest(key) => {
-                info!("Received DataRequest for key {:?}", key);
-                if let Some(value) = self.tree.data.read().unwrap().get(key).cloned() {
-                    let state_clone = self.state.clone();
-                    let id_clone = self.identifier.connection_info().clone();
-                    let neighbor_clone = neighbor.clone();
-                    let protocol_id = msg.protocol();
-                    let key_clone = key.clone();
-
-                    spawn!({
-                        let response = MerkleTreeMessage::new(
-                            protocol_id,
-                            MerkleTreeMessageType::new(MerkleTreeMessageTypeValues::DataResponse),
-                            MerkleTreeMessageWrapper::DataResponse(key_clone, value),
-                        );
-                        let _ = state_clone
-                            .send_through_socket(
-                                id_clone,
-                                Box::new(neighbor_clone),
-                                Box::new(response),
-                            )
-                            .await;
-                    });
-                }
+                self.handle_data_request(msg.protocol(), neighbor, key);
             }
             MerkleTreeMessageWrapper::DataResponse(key, value) => {
-                info!(
-                    "Received DataResponse for key {:?}. Inserting into local tree.",
-                    key
-                );
-                self.tree.insert(key.clone(), value.clone());
+                self.handle_data_response(key, value);
             }
-            _ => info!("Received unknwon message type from {:?}", neighbor),
+            _ => info!("Received unknown message type from {:?}", neighbor),
         }
+    }
+
+    fn handle_sync_root(
+        &self,
+        protocol_id: Option<u64>,
+        neighbor: NodeAddress,
+        root_hash: &[u8; 32],
+    ) {
+        let local_root = self.tree.get_root_hash();
+        if local_root != *root_hash {
+            info!("Root hash mismatch. Requesting children nodes sync.");
+
+            let state_clone = self.state.clone();
+            let id_clone = self.identifier.connection_info().clone();
+            let neighbor_clone = neighbor.clone();
+
+            spawn!({
+                let request_left = MerkleTreeMessage::new(
+                    protocol_id.clone(),
+                    MerkleTreeMessageType::new(MerkleTreeMessageTypeValues::SyncNodeRequest),
+                    MerkleTreeMessageWrapper::SyncNodeRequest("root-left".to_string()),
+                );
+                let _ = state_clone
+                    .send_through_socket(
+                        id_clone.clone(),
+                        Box::new(neighbor_clone.clone()),
+                        Box::new(request_left),
+                    )
+                    .await;
+
+                let request_right = MerkleTreeMessage::new(
+                    protocol_id,
+                    MerkleTreeMessageType::new(MerkleTreeMessageTypeValues::SyncNodeRequest),
+                    MerkleTreeMessageWrapper::SyncNodeRequest("root-right".to_string()),
+                );
+                let _ = state_clone
+                    .send_through_socket(
+                        id_clone,
+                        Box::new(neighbor_clone),
+                        Box::new(request_right),
+                    )
+                    .await;
+            });
+        } else {
+            info!("Root hash match. In sync.");
+        }
+    }
+
+    fn handle_sync_node_request(
+        &self,
+        protocol_id: Option<u64>,
+        neighbor: NodeAddress,
+        node_id: &String,
+    ) {
+        info!("Received SyncNodeRequest from {:?}", node_id);
+        let root_hash = self.tree.get_root_hash();
+
+        let (node_hash, parent_hash, node_key) =
+            if let Some((node, p_hash)) = self.tree.get_node(node_id) {
+                (node.hash, p_hash, node.key)
+            } else {
+                ([0; 32], None, None)
+            };
+
+        let state_clone = self.state.clone();
+        let id_clone = self.identifier.connection_info().clone();
+        let node_id_clone = node_id.clone();
+
+        spawn!({
+            let response = MerkleTreeMessage::new(
+                protocol_id,
+                MerkleTreeMessageType::new(MerkleTreeMessageTypeValues::SyncNodeResponse),
+                MerkleTreeMessageWrapper::SyncNodeResponse(
+                    node_id_clone,
+                    root_hash,
+                    node_hash,
+                    parent_hash,
+                    node_key,
+                ),
+            );
+            let _ = state_clone
+                .send_through_socket(id_clone, Box::new(neighbor), Box::new(response))
+                .await;
+        });
+    }
+
+    fn handle_sync_node_response(
+        &self,
+        protocol_id: Option<u64>,
+        neighbor: NodeAddress,
+        node_id: &String,
+        hash: &[u8; 32],
+        remote_key: &Option<String>,
+    ) {
+        info!("Received SyncNodeResponse for {:?}", node_id);
+        let local_node = self.tree.get_node(node_id);
+        let local_hash = local_node.map_or([0; 32], |(n, _)| n.hash);
+
+        if local_hash != *hash {
+            let state_clone = self.state.clone();
+            let id_clone = self.identifier.connection_info().clone();
+            let neighbor_clone = neighbor.clone();
+            let node_id_clone = node_id.clone();
+
+            if let Some(key) = remote_key {
+                info!("Leaf node mismatch. Requesting data for key: {:?}", key);
+                let key_clone = key.clone();
+                spawn!({
+                    let request = MerkleTreeMessage::new(
+                        protocol_id,
+                        MerkleTreeMessageType::new(MerkleTreeMessageTypeValues::DataRequest),
+                        MerkleTreeMessageWrapper::DataRequest(key_clone),
+                    );
+                    let _ = state_clone
+                        .send_through_socket(id_clone, Box::new(neighbor_clone), Box::new(request))
+                        .await;
+                });
+            } else if *hash != [0; 32] {
+                info!(
+                    "Internal node mismatch at {:?}. Requesting children.",
+                    node_id
+                );
+                spawn!({
+                    let request_left = MerkleTreeMessage::new(
+                        protocol_id.clone(),
+                        MerkleTreeMessageType::new(MerkleTreeMessageTypeValues::SyncNodeRequest),
+                        MerkleTreeMessageWrapper::SyncNodeRequest(format!(
+                            "{}-left",
+                            node_id_clone
+                        )),
+                    );
+                    let _ = state_clone
+                        .send_through_socket(
+                            id_clone.clone(),
+                            Box::new(neighbor_clone.clone()),
+                            Box::new(request_left),
+                        )
+                        .await;
+
+                    let request_right = MerkleTreeMessage::new(
+                        protocol_id,
+                        MerkleTreeMessageType::new(MerkleTreeMessageTypeValues::SyncNodeRequest),
+                        MerkleTreeMessageWrapper::SyncNodeRequest(format!(
+                            "{}-right",
+                            node_id_clone
+                        )),
+                    );
+                    let _ = state_clone
+                        .send_through_socket(
+                            id_clone,
+                            Box::new(neighbor_clone),
+                            Box::new(request_right),
+                        )
+                        .await;
+                });
+            }
+        }
+    }
+
+    fn handle_data_request(&self, protocol_id: Option<u64>, neighbor: NodeAddress, key: &String) {
+        info!("Received DataRequest for key {:?}", key);
+        if let Some(value) = self.tree.data.read().unwrap().get(key).cloned() {
+            let state_clone = self.state.clone();
+            let id_clone = self.identifier.connection_info().clone();
+            let key_clone = key.clone();
+
+            spawn!({
+                let response = MerkleTreeMessage::new(
+                    protocol_id,
+                    MerkleTreeMessageType::new(MerkleTreeMessageTypeValues::DataResponse),
+                    MerkleTreeMessageWrapper::DataResponse(key_clone, value),
+                );
+                let _ = state_clone
+                    .send_through_socket(id_clone, Box::new(neighbor), Box::new(response))
+                    .await;
+            });
+        }
+    }
+
+    fn handle_data_response(&self, key: &String, value: &String) {
+        info!(
+            "Received DataResponse for key {:?}. Inserting into local tree.",
+            key
+        );
+        self.tree.insert(key.clone(), value.clone());
     }
 }
 
